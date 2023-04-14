cd22ec7311c6e62bce5c0d1e49b9cfaf1c4987cc
Ilya Fakhrutdinov <bekamez@gmail.com>
2023-04-14 15:48:28 +0500

    feat: add `encode-line` solution

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given a string, return its encoding version.
  *
@@ -10,9 +8,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const result = [];
+
+  let count = 1;
+  for (let i = 1; i < str.length; i++) {
+    const prev = str[i - 1];
+    const curr = str[i];
+    if (curr === prev) {
+      count += 1;
+    } else {
+      result.push([prev, count]);
+      count = 1;
+    }
+    if (i === str.length - 1) {
+      result.push([curr, curr === prev ? count : 1]);
+    }
+  }
+  return result.map(([key, val]) => (
+    `${(val === 1) ? key : `${val}${key}`}`
+  )).join('');
 }
 
 module.exports = {
